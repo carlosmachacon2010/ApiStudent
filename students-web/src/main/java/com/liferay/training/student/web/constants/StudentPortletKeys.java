1de74b4b098b61d331d6aca0cd8e1757fb144ce4
carlosmachacon2010 <carlos.machacon@vasslatam.com>
2021-08-25 15:04:25 -0500

    module web
    create module web student

---
+++ students-web/src/main/java/com/liferay/training/student/web/constants/StudentPortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferay.training.student.web.constants;
+
+/**
+ * @author carlos.machacon
+ */
+public class StudentPortletKeys {
+
+	public static final String STUDENT =
+		"com_liferay_training_student_web_StudentPortlet";
+
+}
---
+++ students-web/src/main/java/com/liferay/training/student/web/portlet/StudentPortlet.java
@@ -0,0 +1,30 @@
+package com.liferay.training.student.web.portlet;
+
+import com.liferay.training.student.web.constants.StudentPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author carlos.machacon
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=Student",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + StudentPortletKeys.STUDENT,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class StudentPortlet extends MVCPortlet {
+}
